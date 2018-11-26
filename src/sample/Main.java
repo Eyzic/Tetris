fe6efa98c05112a0517e739902cca8efaf1f2ab2
Eyzic <einar@ingemarsson.nu>
2018-11-26 21:32:18 +0100

    First draft

---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+
+        Rectangle r = new Rectangle(20,20);
+        root.
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
